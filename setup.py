a5874c638e033f622295905242a12ab594042429
fwoerister <ffwoerister@gmail.com>
2020-09-04 21:00:06 +0200

    cleanup code

--- setup.py
+++ setup.py
@@ -9,5 +9,21 @@ setup(
     author_email='''e1126205@student.tuwien.ac.at''',
 
     packages=find_packages(),
-    namespace_packages=['evaluation']
+    namespace_packages=['evaluation'],
+
+    license='GNU Affero General Public License v3 or later (AGPLv3+)',
+    classifiers=[
+        # How mature is this project? Common values are
+        # 3 - Alpha
+        # 4 - Beta
+        # 5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.8',
+    ],
 )
