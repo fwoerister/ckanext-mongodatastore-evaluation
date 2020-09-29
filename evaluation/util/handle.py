8c499721f291336b423cef079dd8a4470ce04578
Florian Wörister <ffwoerister@gmail.com>
2020-09-29 15:59:42 +0200

    easyhandle package added

--- evaluation/util/handle.py
+++ evaluation/util/handle.py
@@ -1,7 +1,7 @@
 import json
 import logging
 
-import requests
+from easyhandle.client import BasicAuthHandleClient
 
 STREAM_TEMPLATE = '{}/storedquery/{}/dump'
 API_TEMPLATE = '{}/api/3/action/querystore_resolve?id={}'
@@ -10,16 +10,17 @@ URL_TEMPLATE = '{}/storedquery/landingpage?id={}'
 logger = logging.getLogger(__name__)
 
 config = json.load(open('config.json', 'r'))
+client = BasicAuthHandleClient.load_from_config(config['handle'])
 
 
-def verify_handle_resolves_to_pid(handle_pid, pid):
-    result = requests.get('{}/api/handles/{}'.format(config['handle']['base_url'], handle_pid))
+def verify_handle_resolves_to_pid(handle_pid, internal_id):
+    result = client.get_handle(handle_pid)
     values = result.json()['values']
 
     for value in values:
         if value['type'] == 'URL':
-            assert value['data']['value'] == URL_TEMPLATE.format(config['ckan']['site_url'], pid)
+            assert value['data']['value'] == URL_TEMPLATE.format(config['ckan']['site_url'], internal_id)
         elif value['type'] == 'API_URL':
-            assert value['data']['value'] == API_TEMPLATE.format(config['ckan']['site_url'], pid)
+            assert value['data']['value'] == API_TEMPLATE.format(config['ckan']['site_url'], internal_id)
         elif value['type'] == 'STREAM_URL':
-            assert value['data']['value'] == STREAM_TEMPLATE.format(config['ckan']['site_url'], pid)
+            assert value['data']['value'] == STREAM_TEMPLATE.format(config['ckan']['site_url'], internal_id)
