0eafa522ce5ce5d7e1f45ef30767db714f5f7120
Florian Wörister <ffwoerister@gmail.com>
2020-10-11 23:51:26 +0200

    refactor test naming

--- evaluation/tests/nonfunctional/__init__.py
+++ evaluation/tests/nonfunctional/__init__.py
@@ -1,7 +1,7 @@
 from evaluation.tests.nonfunctional.nftc1_performance_query_current_state import PerformanceQueryCurrentStateTest
-from evaluation.tests.nonfunctional.nftc2_performance_stored_query import PerformanceStoredQueryTest
-from evaluation.tests.nonfunctional.nftc3_performance_with_index import PerformanceIndexUsage
-from evaluation.tests.nonfunctional.nftc4_performance_nonversioned import PerformanceNonVersionedMongoResource
+from evaluation.tests.nonfunctional.nftc2_performance_nonversioned import PerformanceNonVersionedMongoResource
+from evaluation.tests.nonfunctional.nftc4_performance_with_index import PerformanceIndexUsage
+from evaluation.tests.nonfunctional.nftc3_performance_stored_query import PerformanceStoredQueryTest
 
 TRACE_DATASET = 'data/datasets/preprocessed_trace'
 TRACE_REMAINING = 'data/datasets/preprocessed_trace_remaining'
@@ -14,10 +14,12 @@ def initialize(results_dir):
     NON_FUNCTIONAL_TESTS.update({
         '1': PerformanceQueryCurrentStateTest(results_dir, 'nftc1_performance_query_current_state',
                                               TRACE_DATASET, 10000, 500000),
-        '2': PerformanceStoredQueryTest(results_dir, 'nftc2_performance_stored_query',
-                                        TRACE_REMAINING, 10000, 500000),
-        '3': PerformanceIndexUsage(results_dir, 'nftc3_performance_with_index',
-                                   TRACE_DATASET, 10000, 500000),
-        '4': PerformanceNonVersionedMongoResource(results_dir, 'nftc4_performance_non_versioned_mongodb',
+        '2': PerformanceNonVersionedMongoResource(results_dir, 'nftc2_performance_non_versioned_mongodb',
                                                   TRACE_DATASET, 10000, 500000),
+        '3': PerformanceStoredQueryTest(results_dir, 'nftc3_performance_stored_query',
+                                        TRACE_REMAINING, 10000, 500000),
+        '4.1': PerformanceIndexUsage(results_dir, 'nftc4_performance_with_index',
+                                   TRACE_DATASET, 10000, 500000, db_type='mongodb'),
+        '4.2': PerformanceIndexUsage(results_dir, 'nftc4_performance_with_index',
+                                   TRACE_DATASET, 10000, 500000, db_type='postgresql')
     })
